1255fc6a0bbba5f658bfe1e3036a29935f5af770
matsuura hiroki <139605971+matsuurahiroki@users.noreply.github.com>
2025-11-11 18:18:14 +0900

    fix: correct schedules controller test URL helpers

--- test/controllers/schedules_controller_test.rb
+++ test/controllers/schedules_controller_test.rb
@@ -2,22 +2,25 @@ require "test_helper"
 
 class SchedulesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get schedules_index_url
+    get schedules_url
     assert_response :success
   end
 
   test "should get new" do
-    get schedules_new_url
+    get new_schedule_url
     assert_response :success
   end
 
   test "should get show" do
-    get schedules_show_url
+    # テスト用に1件作る
+    schedule = Schedule.create!(title: "Test", start_on: Date.today, end_on: Date.today)
+    get schedule_url(schedule)
     assert_response :success
   end
 
   test "should get edit" do
-    get schedules_edit_url
+    schedule = Schedule.create!(title: "Test", start_on: Date.today, end_on: Date.today)
+    get edit_schedule_url(schedule)
     assert_response :success
   end
 end
